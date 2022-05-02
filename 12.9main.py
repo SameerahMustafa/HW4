c355bd15fd10557c309dcdc39a72c10ccbc231b3
SameerahMustafa <70537003+SameerahMustafa@users.noreply.github.com>
2022-05-01 22:22:22 -0500

    Add files via upload

---
+++ 12.9main.py
@@ -0,0 +1,14 @@
+#Sameerah Mustafa
+#1584330
+
+parts = input().split()
+name = parts[0]
+
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+        print("{} {}".format(name, age))
+    except:
+        print("{} 0".format(name))
+    parts = input().split()
+    name = parts[0]
